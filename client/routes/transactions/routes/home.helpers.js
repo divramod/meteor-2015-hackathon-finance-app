60961193ee32594c4439bc189fefbbdc9482c2cb
tilmnn <dev@tilmannb.de>
2015-10-10 19:56:25 +0200

    Added Hammer.js support

--- client/routes/transactions/routes/home.helpers.js
+++ client/routes/transactions/routes/home.helpers.js
@@ -1,4 +1,21 @@
 // =========== [ transactionsHome helpers ] ===========
 Template.transactionsHome.helpers({
+  templateGestures: {
+    'panleft div': function(event, templateInstance) {
+        console.log(event.type + ' dected');
 
+      /* `event` is the Hammer.js event object */
+      /* `templateInstance` is the `Blaze.TemplateInstance` */
+      /* `this` is the data context of the element in your template, so in this case `someField` from `someArray` in the template */
+    },
+
+    'panright div': function(event, TemplateInstance) {
+        console.log(event.type + ' dected');
+    }
+  },
+  categories: [
+    {name: "Category 1"},
+    {name: "Category 2"},
+    {name: "Category 3"}
+  ]
 });
