8db9b94d4a7abe8e488eda0e9a9c2cc014765ce2
xanatas <pscigala@gmail.com>
2015-10-10 21:51:23 +0200

    schema update 0.1

--- client/routes/bookings/templates/insert.helpers.js
+++ client/routes/bookings/templates/insert.helpers.js
@@ -1,4 +1,22 @@
 // =========== [ bookingsInsert helpers ] ===========
 Template.bookingsInsert.helpers({
 
+		catOptions: function () {
+		array = []
+		result = categories.find()
+		result.forEach(function (area) {
+			array.push({label: area.name, value: area._id})
+		});
+		return array;
+	},
+
+		areaOptions: function () {
+		array = []
+		result = areas.find()
+		result.forEach(function (area) {
+			array.push({label: area.name, value: area._id})
+		});
+		return array;
+	}
+
 });
