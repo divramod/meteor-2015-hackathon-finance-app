cb731e3855aeac2052b5cb22b8dbe541cbb80ef2
xanatas <pscigala@gmail.com>
2015-10-11 11:50:35 +0200

    booklingsDone

--- client/routes/bookings/templates/insert.helpers.js
+++ client/routes/bookings/templates/insert.helpers.js
@@ -1,4 +1,32 @@
 // =========== [ bookingsInsert helpers ] ===========
 Template.bookingsInsert.helpers({
 
+		catOptions: function () {
+		array = []
+		result = categories.find()
+		result.forEach(function (area) {
+			array.push({label: area.name, value: area._id})
+		});
+		return array;
+	},
+
+		areaOptions: function () {
+		array = []
+		result = areas.find()
+		result.forEach(function (area) {
+			array.push({label: area.name, value: area._id})
+		});
+		return array;
+	},
+
+		transactionsOptions: function () {
+		array = []
+		result = transactions.find()
+		result.forEach(function (area) {
+			array.push({label: area._id, value: area._id})
+		});
+		return array;
+	}
+
+
 });
