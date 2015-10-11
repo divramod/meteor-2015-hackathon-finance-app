8909b736c12f67da5124f151f2d91bff88bc5a48
divramod <divramod@gmail.de>
2015-10-11 11:28:16 +0200

    update

--- client/routes/areas/templates/update.helpers.js
+++ client/routes/areas/templates/update.helpers.js
@@ -1,11 +1,11 @@
 // =========== [ areasUpdate helpers ] ===========
 Template.areasUpdate.helpers({
-	groupOptions: function () {
-		array = []
-		result = groups.find()
-		result.forEach(function (group) {
-			array.push({label: group.title, value: group._id})
-		});
-		return array;
-	}
+  groupOptions: function () {
+    array = []
+      result = groups.find()
+      result.forEach(function (group) {
+        array.push({label: group.title, value: group._id})
+      });
+    return array;
+  }
 });
